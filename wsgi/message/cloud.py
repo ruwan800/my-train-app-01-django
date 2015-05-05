8c3ca2cb6c15f72f03d93a4d7c8254530e6360c8
Ruwan Jayasinghe <ruwan800@gmail.com>
2015-05-06 05:19:27 +0530

    fix 02- gcm implementation

--- wsgi/message/cloud.py
+++ wsgi/message/cloud.py
@@ -4,9 +4,9 @@ from advanced.cloud import send
 
 def send_gcm_messages(thread, sender, time):
     subscriptions = Contact.objects.filter(thread=thread, favourite=True)
-    recipients = [x.user.key for x in subscriptions]
-    if sender.key in recipients:
-        recipients.remove(sender.key)
+    recipients = [x.user for x in subscriptions]
+    if sender in recipients:
+        recipients.remove(sender)
     data = dict()
     data['thread'] = thread.get_reference().get_name()
     data['reference'] = thread.id
